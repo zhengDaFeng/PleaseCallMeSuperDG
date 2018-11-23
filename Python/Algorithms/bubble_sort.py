76cf49240432c5b1917dd05a3421282c93faa0cd
zhengDaFeng <1191080886@qq.com>
2018-11-23 14:37:23 +0800

    Update Python/Algorithms/bubble_sort.py

--- Python/Algorithms/bubble_sort.py
+++ Python/Algorithms/bubble_sort.py
@@ -1,1 +1,12 @@
+def bubble_sort(collection):
+    length = len(collection)
+    for i in range(length-1):
+        swapped = False
+        for j in range(length-1-i):
+            if collection[j] > collection[j+1]:
+                swapped = True
+                collection[j], collection[j+1] = collection[j+1], collection[j]
+        if not swapped: break
+    return collection
 
+print(bubble_sort([1, 4, 2, 7]))
