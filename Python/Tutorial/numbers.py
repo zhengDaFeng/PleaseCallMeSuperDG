54b9e2033c92d8587bdc5974bbf5f7660e0146f7
zhengDaFeng <1191080886@qq.com>
2018-11-23 17:25:12 +0800

    Create Python/Tutorial/numbers.py

---
+++ Python/Tutorial/numbers.py
@@ -0,0 +1,7 @@
+print('加法：2 + 2 = ', 2 + 2)
+print('减法：2 - 2 = ', 2 - 2)
+print('乘法：2 * 2 = ', 2 * 2)
+print('除法：5 / 2 = ', 5 / 2)    # division always returns a floating point number
+print('取余：5 % 2 = ', 5 % 2)
+print('取整：5 // 2 = ', 5 // 2)
+print('平方：5 ** 2 = ', 5 ** 2)
