f793569bdc2f7da2782bade9892d3c3cc2b0d4db
zhengDaFeng <1191080886@qq.com>
2018-11-23 17:57:14 +0800

    Create Python/Tutorial/string.py via git

---
+++ Python/Tutorial/strings.py
@@ -0,0 +1,20 @@
+print('wrapped by single quotes')   # single quotes
+print("wrapped bu double quotes")   # double quotes
+print('She doesn\'t like me!') # use \' to escape the single quote
+print("I'm fond of her") # use double quotes so you can show the single
+print('"Yes," they said.')
+print("\"Yes,\" they said.")
+s = 'First line. \nSecond line.'
+print(s)
+path1 = 'C:\some\name'
+path2 = r'C:\some\name'
+print(path1)
+print(path2)
+print("""
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+print(3 * 'un' + 'ium')
+print('Put several strings within parentheses '
+      'to have them joined together.')
